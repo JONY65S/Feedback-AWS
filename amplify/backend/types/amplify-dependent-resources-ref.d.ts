34b49cc66ca782d1e0d64a90d6b9e25841bfcbe8
JONY65S <jonathan.baltazar85@outlook.com>
2025-01-16 20:37:09 -0600

    storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -11,7 +11,7 @@ export type AmplifyDependentResourcesAttributes = {
     }
   },
   "storage": {
-    "AmplifyStorage": {
+    "feedbackInsight": {
       "BucketName": "string",
       "Region": "string"
     }
