9a043cc0be10713dce2d3f78784a66b3dcdd9236
JONY65S <jonathan.baltazar85@outlook.com>
2025-01-16 20:26:28 -0600

    backup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import LiveDemo from "./pages/LiveDemo";
+import Livedemo from "./pages/LiveDemo";
 import Insights from "./pages/Insights";
 // import About from "./pages/About";
 import Features from "./pages/Features";
@@ -9,13 +9,14 @@ import Contact from "./pages/Contact";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
+
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/live-demo" element={<LiveDemo />} />
+        <Route path="/live-demo" element={<Livedemo />} />
         <Route path="/insights" element={<Insights />} />
         <Route path="/features" element={<Features />} />
         <Route path="/contact" element={<Contact />} />
---
+++ src/amplify-config.js
@@ -0,0 +1,11 @@
+// import { Amplify, Storage } from 'aws-amplify';
+
+// Amplify.configure({
+//   Storage: {
+//     AWSS3: {
+//       bucket: 'smart-feedback-bucket',  // Nombre de tu bucket S3
+//       region: 'us-west-2',            // Región donde está ubicado el bucket
+//       isObjectLockEnabled: true       // Habilitar el bloqueo de objetos (opcional)
+//     }
+//   }
+// });
