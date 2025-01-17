02c49b48d6e1fd39992b2e8bea3401936ca41b0f
JONY65S <jonathan.baltazar85@outlook.com>
2025-01-17 00:11:12 -0600

    add quiz

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import './index.css'
 import App from './App.tsx'
-// import { Amplify } from 'aws-amplify';
-// import './amplify-config'; 
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports'; // Ajusta la ruta si es necesario
+
+Amplify.configure(awsExports); // Asegúrate de que esta línea esté presente y correcta
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- src/pages/LiveDemo.tsx
+++ src/pages/LiveDemo.tsx
@@ -1,68 +1,72 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome, faInfoCircle, faPlayCircle, faUsers, faPhone } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from 'react';
+import { uploadData } from 'aws-amplify/storage';
 
-const Navbar: React.FC = () => {
+const LiveDemo = () => {
+  const [comment, setComment] = useState('');
+  const [isUploading, setIsUploading] = useState(false);
+
+  const handleCommentChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setComment(event.target.value);
+  };
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (!comment) {
+      alert('Por favor ingrese un comentario.');
+      return;
+    }
+
+    const timestamp = new Date().toISOString(); // Timestamp en formato ISO
+    const commentData = {
+      comments: [
+        {
+          id: `${Date.now()}`, // ID único basado en el tiempo
+          comment,
+          timestamp,
+        },
+      ],
+    };
+
+    const jsonData = JSON.stringify(commentData); // Convertir el objeto a JSON
+
+    try {
+      setIsUploading(true);
+
+      // Subir el archivo JSON al bucket S3
+      const fileName = `comments/comment_${timestamp}.json`; // Nombre del archivo basado en la fecha y comentario
+      const result = await uploadData({
+        path: fileName, // Ruta dentro del bucket
+        data: new Blob([jsonData], { type: 'application/json' }), // Crear un Blob con los datos JSON
+      }).result;
+
+      console.log('Archivo subido exitosamente:', result);
+      alert('Comentario subido exitosamente');
+    } catch (error) {
+      console.error('Error al subir el archivo:', error);
+      alert('Hubo un error al subir el comentario.');
+    } finally {
+      setIsUploading(false);
+    }
+  };
+
   return (
-    <nav className="bg-gradient-to-r from-gray-900 via-gray-800 to-black py-4 shadow-md">
-      <div className="max-w-screen-xl mx-auto flex justify-between items-center px-8 md:px-16">
-        {/* Logo */}
-        <Link to="/" className="text-4xl font-semibold text-white tracking-tight hover:text-yellow-400 transition-all ease-in-out duration-300 transform hover:scale-105">
-          InSightful
-        </Link>
-
-        {/* Menú */}
-        <ul className="flex space-x-6 md:space-x-10">
-          <li>
-            <Link
-              to="/"
-              className="flex items-center text-white text-lg font-medium px-4 py-2 rounded-lg hover:bg-yellow-500 hover:text-gray-900 transition-all ease-in-out duration-300"
-            >
-              <FontAwesomeIcon icon={faHome} className="h-5 w-5 mr-2 text-gray-300 hover:text-white" />
-              Inicio
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="features"
-              className="flex items-center text-white text-lg font-medium px-4 py-2 rounded-lg hover:bg-yellow-500 hover:text-gray-900 transition-all ease-in-out duration-300"
-            >
-              <FontAwesomeIcon icon={faInfoCircle} className="h-5 w-5 mr-2 text-gray-300 hover:text-white" />
-              Proceso
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="#explorar"
-              className="flex items-center text-white text-lg font-medium px-4 py-2 rounded-lg hover:bg-yellow-500 hover:text-gray-900 transition-all ease-in-out duration-300"
-            >
-              <FontAwesomeIcon icon={faPlayCircle} className="h-5 w-5 mr-2 text-gray-300 hover:text-white" />
-              Explorar
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="#impacto"
-              className="flex items-center text-white text-lg font-medium px-4 py-2 rounded-lg hover:bg-yellow-500 hover:text-gray-900 transition-all ease-in-out duration-300"
-            >
-              <FontAwesomeIcon icon={faUsers} className="h-5 w-5 mr-2 text-gray-300 hover:text-white" />
-              Impacto
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="#conversa"
-              className="flex items-center text-white text-lg font-medium px-4 py-2 rounded-lg hover:bg-yellow-500 hover:text-gray-900 transition-all ease-in-out duration-300"
-            >
-              <FontAwesomeIcon icon={faPhone} className="h-5 w-5 mr-2 text-gray-300 hover:text-white" />
-              Conversa
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <div>
+      <h2>Deja tu comentario</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Escribe tu comentario"
+          value={comment}
+          onChange={handleCommentChange}
+          required
+        />
+        <button type="submit" disabled={isUploading}>
+          {isUploading ? 'Subiendo...' : 'Subir Comentario'}
+        </button>
+      </form>
+    </div>
   );
 };
 
-export default Navbar;
+export default LiveDemo;
