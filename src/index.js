c80e8d66214ce66f251b50215f9d933986666e1a
JONY65S <jonathan.baltazar85@outlook.com>
2025-01-17 00:42:33 -0600

    resolve fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import { Amplify } from "aws-amplify";
-import awsExports from './aws-exports';
-Amplify.configure(awsExports);
+import awsmobile from './aws-exports';
+Amplify.configure(awsmobile);
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 import './index.css'
 import App from './App.tsx'
 import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports'; // Ajusta la ruta si es necesario
+import awsmobile from 'aws-exports';
 
-Amplify.configure(awsExports); // Asegúrate de que esta línea esté presente y correcta
+
+Amplify.configure(awsmobile);
+
+
+// Asegúrate de que esta línea esté presente y correcta
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
