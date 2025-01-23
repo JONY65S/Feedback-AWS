8a5a50f70dc5b7c24ffcc6824e50979888006ab5
JONY65S <jonathan.baltazar85@outlook.com>
2025-01-22 22:31:03 -0600

    add form

--- src/pages/LiveDemo.tsx
+++ src/pages/LiveDemo.tsx
@@ -2,18 +2,36 @@ import React, { useEffect, useState } from 'react';
 import { uploadData } from 'aws-amplify/storage';
 import { v4 as uuidv4 } from 'uuid';
 
+// Definimos las interfaces de los datos
+interface City {
+  id: number;
+  name: string;
+}
+
+interface State {
+  id: number;
+  name: string;
+  cities: City[];
+}
+
+interface Country {
+  id: number;
+  name: string;
+  states: State[];
+}
+
 const LiveDemo = () => {
   const [comment, setComment] = useState('');
   const [edad, setEdad] = useState('');
-  const [selectedCountry, setSelectedCountry] = useState(null);
-  const [selectedState, setSelectedState] = useState(null);
-  const [selectedCity, setSelectedCity] = useState(null);
-  const [countries, setCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState<Country | null>(null);
+  const [selectedState, setSelectedState] = useState<State | null>(null);
+  const [selectedCity, setSelectedCity] = useState<City | null>(null);
+  const [countries, setCountries] = useState<Country[]>([]);
   const [isUploading, setIsUploading] = useState(false);
   const [notification, setNotification] = useState({ message: '', type: '' });
 
+  // Cargar los datos desde el archivo JSON o API
   useEffect(() => {
-    // Simulación de carga de datos de país, estado y ciudad
     const loadCountries = async () => {
       const response = await fetch('/country.json');
       const data = await response.json();
@@ -22,15 +40,11 @@ const LiveDemo = () => {
     loadCountries();
   }, []);
 
-  const handleCommentChange = (event) => {
-    setComment(event.target.value);
-  };
+  const handleCommentChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => setComment(event.target.value);
 
-  const handleEdadChange = (event) => {
-    setEdad(event.target.value);
-  };
+  const handleEdadChange = (event: React.ChangeEvent<HTMLSelectElement>) => setEdad(event.target.value);
 
-  const handleCountryChange = (event) => {
+  const handleCountryChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const countryId = parseInt(event.target.value, 10);
     const country = countries.find((c) => c.id === countryId);
     setSelectedCountry(country || null);
@@ -38,27 +52,27 @@ const LiveDemo = () => {
     setSelectedCity(null);
   };
 
-  const handleStateChange = (event) => {
+  const handleStateChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const stateId = parseInt(event.target.value, 10);
     const state = selectedCountry?.states.find((s) => s.id === stateId);
     setSelectedState(state || null);
     setSelectedCity(null);
   };
 
-  const handleCityChange = (event) => {
+  const handleCityChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const cityId = parseInt(event.target.value, 10);
     const city = selectedState?.cities.find((c) => c.id === cityId);
     setSelectedCity(city || null);
   };
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = async (event: { preventDefault: () => void; }) => {
     event.preventDefault();
-
+  
     if (!comment || !edad || !selectedCountry || !selectedState || !selectedCity) {
       setNotification({ message: 'Por favor complete todos los campos.', type: 'error' });
       return;
     }
-
+  
     const timestamp = new Date().toISOString();
     const commentData = {
       comments: [
@@ -67,40 +81,56 @@ const LiveDemo = () => {
           comment,
           timestamp,
           edad,
-          pais: selectedCountry.name,
-          estado: selectedState.name,
-          ciudad: selectedCity.name,
+          pais: selectedCountry?.name || '',
+          estado: selectedState?.name || '',
+          ciudad: selectedCity?.name || '',
         },
       ],
     };
-
+  
     const jsonData = JSON.stringify(commentData);
-
+  
     try {
       setIsUploading(true);
       const uniqueFolio = uuidv4();
       const fileName = `comments/${uniqueFolio}.json`;
-
+  
       const result = await uploadData({
         path: fileName,
         data: new Blob([jsonData], { type: 'application/json' }),
       }).result;
-
+  
       console.log('Archivo subido exitosamente:', result);
       setNotification({ message: 'Comentario subido exitosamente', type: 'success' });
+  
+      // Limpiar los campos después de un retraso
+      setTimeout(() => {
+        setComment('');
+        setEdad('');
+        setSelectedCountry(null);
+        setSelectedState(null);
+        setSelectedCity(null);
+        setNotification({ message: '', type: '' }); // Limpiar notificación
+      }, 3000); // La notificación desaparece después de 3 segundos
+  
     } catch (error) {
       console.error('Error al subir el archivo:', error);
       setNotification({ message: 'Hubo un error al subir el comentario.', type: 'error' });
+  
+      // Limpiar notificación después de 3 segundos
+      setTimeout(() => setNotification({ message: '', type: '' }), 3000);
     } finally {
       setIsUploading(false);
     }
   };
+  
 
   return (
     <div className="bg-[#FAF3E0] py-6 h-screen flex justify-center items-center">
       <div className="max-w-lg mx-auto p-8 bg-gray-900 rounded-xl shadow-xl">
         <h2 className="text-3xl font-bold text-center text-yellow-500 mb-6">¡Déjanos tu Comentario!</h2>
         <form onSubmit={handleSubmit} className="space-y-6">
+          {/* Edad */}
           <div>
             <label htmlFor="edad" className="block text-white mb-1">Selecciona tu edad:</label>
             <select
@@ -116,6 +146,7 @@ const LiveDemo = () => {
             </select>
           </div>
 
+          {/* País */}
           <div>
             <label htmlFor="country" className="block text-white mb-1">Selecciona tu país:</label>
             <select
@@ -130,7 +161,8 @@ const LiveDemo = () => {
             </select>
           </div>
 
-          {selectedCountry && (
+          {/* Estado */}
+          {selectedCountry?.states && (
             <div>
               <label htmlFor="state" className="block text-white mb-1">Selecciona tu estado:</label>
               <select
@@ -146,7 +178,8 @@ const LiveDemo = () => {
             </div>
           )}
 
-          {selectedState && (
+          {/* Ciudad */}
+          {selectedState?.cities && (
             <div>
               <label htmlFor="city" className="block text-white mb-1">Selecciona tu ciudad:</label>
               <select
@@ -162,6 +195,7 @@ const LiveDemo = () => {
             </div>
           )}
 
+          {/* Comentario */}
           <div>
             <label htmlFor="comment" className="block text-white mb-1">Escribe tu comentario:</label>
             <textarea
@@ -175,6 +209,7 @@ const LiveDemo = () => {
             />
           </div>
 
+          {/* Botón */}
           <button
             type="submit"
             disabled={isUploading}
@@ -186,6 +221,7 @@ const LiveDemo = () => {
           </button>
         </form>
 
+        {/* Notificación */}
         {notification.message && (
           <div
             className={`fixed top-5 left-1/2 transform -translate-x-1/2 w-96 p-4 rounded-lg shadow-lg transition-all duration-300 ease-in-out ${
